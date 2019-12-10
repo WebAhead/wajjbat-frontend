e579c87985fc62aa1dc335b2583c1c78d5cd3708
HosenAgbaria <hosen.agbaria@hotmail.com>
2019-12-10 12:19:25 +0200

    home page nav bar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.scss";
+export default function index() {
+  return (
+    <div className="navBar">
+      <div className="changeLanguage"></div>
+      <div className="signUp"></div>
+      <div className="login"></div>
+    </div>
+  );
+}
