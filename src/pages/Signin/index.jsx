212b9c2f39cd117268c4599ce6191336c3bed9df
omriza5 <omriza5@gmail.com>
2019-12-16 14:45:59 +0200

    modify: sign in page close #9

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
 import FacebookLogin from "react-facebook-login";
 import GoogleLogin from "react-google-login";
