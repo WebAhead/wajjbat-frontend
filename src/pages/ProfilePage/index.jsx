d4c8e27fc097db8ab713dabe46850f700e56e37c
logicx1 <uninajone@gmail.com>
2020-03-23 15:07:53 +0200

    Adds a logout button and functionality , back button in auth page rel #5 closes #19

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import axios from 'axios';
 import './styles.scss';
@@ -8,53 +8,61 @@ import ReviewCard from 'components/Cards/ReviewCard';
 const endPointUrl = process.env.REACT_APP_API_URL;
 
 function ProfilePage(props) {
-    const [reviews, setReviews] = useState('');
-    const [userDetails, setUserDetails] = useState({});
+  const [reviews, setReviews] = useState('');
+  const [userDetails, setUserDetails] = useState({});
+  const [err, setErr] = useState(null);
+  const history = useHistory();
+  useEffect(() => {
+    axios
+      .get(`${endPointUrl}/api/getUserReviews`, { withCredentials: 'true' })
+      .then(res => {
+        setReviews(res.data.reviews);
+        setUserDetails({ ...res.data.userDetails });
+      })
+      .catch(error => {
+        setErr(error);
+        return error;
+      });
+  }, []);
 
-    useEffect(() => {
-        axios
-            .get(`${endPointUrl}/api/getUserReviews`, { withCredentials: 'true' })
-            .then((res) => {
-                setReviews(res.data.reviews);
-                setUserDetails({ ...res.data.userDetails });
-            })
-            .catch((error) => error);
-    }, []);
+  if (err) {
+    if (err.response.status === 403) history.push('/signin');
+  }
 
-    if (!userDetails.firstName) {
-        return 'Loading...';
-    }
+  if (!userDetails.firstName) {
+    return 'Loading...';
+  }
 
-    return (
-        <div>
-            <div className="navbar-container">
-                <button className="navButton">
-                    <Link to="/profile">
-                        <FormattedMessage id="Profile" />
-                    </Link>
-                </button>
+  return (
+    <div>
+      <div className='navbar-container'>
+        <button className='navButton'>
+          <Link to='/profile'>
+            <FormattedMessage id='Profile' />
+          </Link>
+        </button>
 
-                <button className="navButton">
-                    <Link to="/profile-business-list">
-                        <FormattedMessage id="Business" />
-                    </Link>
-                </button>
-            </div>
-            <div className="profile-container">
-                <img src={userDetails.profilePic} alt="" />
-                <p>{userDetails.lastName}</p>
-                <p>{userDetails.firstName}</p>
-            </div>
-            {reviews.map((currentReview) => (
-                <ReviewCard
-                    fullname={currentReview.businessname}
-                    rating={currentReview.rating}
-                    review_body={currentReview.reviewbody}
-                    dateCreated={currentReview.reviewdate}
-                />
-            ))}
-        </div>
-    );
+        <button className='navButton'>
+          <Link to='/profile-business-list'>
+            <FormattedMessage id='Business' />
+          </Link>
+        </button>
+      </div>
+      <div className='profile-container'>
+        <img src={userDetails.profilePic} alt='' />
+        <p>{userDetails.lastName}</p>
+        <p>{userDetails.firstName}</p>
+      </div>
+      {reviews.map(currentReview => (
+        <ReviewCard
+          fullname={currentReview.businessname}
+          rating={currentReview.rating}
+          review_body={currentReview.reviewbody}
+          dateCreated={currentReview.reviewdate}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProfilePage;
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import axios from 'axios';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { FormattedMessage, injectIntl } from 'react-intl';
+import { useHistory } from 'react-router-dom';
 import './style.scss';
 
 const endPointUrl = process.env.REACT_APP_API_URL;
@@ -10,78 +14,105 @@ const facebookId = process.env.REACT_APP_FACEBOOK_ID;
 const googleId = process.env.REACT_APP_GOOGLE_ID;
 
 function Signin({ intl }) {
-    const responseFacebook = async (response) => {
-        if(!response.id) return;
-        const { name, email, id } = response;
-        const { url } = response.picture.data;
+  const history = useHistory();
+  const back = () => {
+    history.goBack();
+  };
 
-        try {
-            await axios.post(`${endPointUrl }/api/oauth/facebook`, {
-                name,
-                email,
-                id,
-                url,
-            }, {
-                withCredentials: true,
-            });
-            window.location = '/';
-        } catch (error) {
-            console.log('Error: ', error);
-        }
-    };
+  const responseFacebook = async response => {
+    if (!response.id) return;
+    const { name, email, id } = response;
+    const { url } = response.picture.data;
 
-    const responseGoogle = async (response) => {
-        if(!response.googleId) return;
-        const { googleId: id, email, name, imageUrl: url } = response.profileObj;
-        try {
-            await axios.post(`${endPointUrl }/api/oauth/google`, {
-                id,
-                email,
-                name,
-                url,
-            }, {
-                withCredentials: true,
-            });
-            window.location = '/';
-        } catch (error) {
-            console.log('Error: ', error);
-        }
-    };
+    try {
+      await axios.post(
+        `${endPointUrl}/api/oauth/facebook`,
+        {
+          name,
+          email,
+          id,
+          url,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      window.location = '/';
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
 
-    const googleMessage = intl.formatMessage({ id: 'Login with Google' })
-    const facebookMessage = intl.formatMessage({ id: 'Login with Facebook' })
+  const responseGoogle = async response => {
+    if (!response.googleId) return;
+    const { googleId: id, email, name, imageUrl: url } = response.profileObj;
+    try {
+      await axios.post(
+        `${endPointUrl}/api/oauth/google`,
+        {
+          id,
+          email,
+          name,
+          url,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      window.location = '/';
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
 
-    return (
-        <>
-            <div className="form-container" style={{ marginTop:'15vh' }}>
-                <h1 className="form-title"><FormattedMessage id="signin" /></h1>
-                <div className="signin-form">
-                    <form className="form-items">
-                        <FacebookLogin
-                            appId={facebookId}
-                            fields="name,email,picture"
-                            callback={responseFacebook}
-                            autoLoad={false}
-                            disableMobileRedirect
-                            icon="fa-facebook"
-                            redirectUri={`${process.env.REACT_APP_URL}/signin`}
-                            textButton={facebookMessage}
-                        />
-                        <br />
-                        <br />
-                        <GoogleLogin
-                            clientId={googleId}
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            autoLoad={false}
-                            buttonText={googleMessage}
-                        />
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+  const useStyles = makeStyles({
+    root: { color: '#21b5a2', height: '40px', width: '40px' },
+  });
+
+  const classes = useStyles();
+
+  const googleMessage = intl.formatMessage({ id: 'Login with Google' });
+  const facebookMessage = intl.formatMessage({ id: 'Login with Facebook' });
+
+  return (
+    <>
+      <div className='form-container' style={{ marginTop: '15vh' }}>
+        <button className='logout-btn' onClick={() => back()}>
+          <ArrowBackOutlinedIcon
+            classes={{
+              root: classes.root,
+            }}
+          />
+        </button>
+        <h1 className='form-title'>
+          <FormattedMessage id='signin' />
+        </h1>
+        <div className='signin-form'>
+          <form className='form-items'>
+            <FacebookLogin
+              appId={facebookId}
+              fields='name,email,picture'
+              callback={responseFacebook}
+              autoLoad={false}
+              disableMobileRedirect
+              icon='fa-facebook'
+              redirectUri={`${process.env.REACT_APP_URL}/signin`}
+              textButton={facebookMessage}
+            />
+            <br />
+            <br />
+            <GoogleLogin
+              clientId={googleId}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              autoLoad={false}
+              buttonText={googleMessage}
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-
-export default injectIntl(Signin)
+export default injectIntl(Signin);
