8fd9dd8147f07f07636b1d8bb7b45fea2cde8f19
logicx1 <uninajone@gmail.com>
2020-03-25 17:53:47 +0200

    Fixes map in create business page,polish choose location component Closes #18

--- src/components/ChooseLocation/index.jsx
+++ src/components/ChooseLocation/index.jsx
@@ -1,46 +1,63 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import path from 'path';
+function ChooseLocation({ userPosition, google, setBusinessLatlng, latLng }) {
+  const mapStyles = {
+    position: 'relative',
+    margin: 'auto',
+    width: '100%',
+    height: '40vh',
+  };
+  console.log(`user pos is : ${userPosition.lat}`);
+  return (
+    <div style={{ minHeight: '40vh' }}>
+      <LoadScript
+        id="add-bzns"
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}
+      >
+        <GoogleMap
+          containerStyle={{
+            height: '50vh',
+            width: '50vw',
+            position: 'relative',
+          }}
+          zoom={13}
+          mapContainerStyle={mapStyles}
+          center={{
+            lat: latLng.lat,
+            lng: latLng.lng,
+          }}
+        >
+          <Marker
+            name={'User location'}
+            position={{
+              lat: +userPosition.lat,
+              lng: +userPosition.lng,
+            }}
+            icon={path.join(__dirname, 'user-location.png')}
+          />
 
-function ChooseLocation({ userPosition, google, changeState, latLng }) {
-    const mapStyles = {
-        position: 'relative',
-        margin: 'auto',
-        marginTop: '20px',
-        width: '100%',
-        height: '50vh',
-    };
-
-    return (
-        <div>
-            <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
-                <GoogleMap
-                    google={google}
-                    containerStyle={{ position: 'relative' }}
-                    zoom={13}
-                    style={mapStyles}
-                    initialCenter={{
-                        lat: userPosition.lat,
-                        lng: userPosition.lng,
-                    }}
-                >
-                    <Marker
-                        onDragend={(event, t, coord) => {
-                            changeState({ lat: coord.latLng.lat(), lng: coord.latLng.lng() });
-                        }}
-                        draggable
-                        position={
-                            latLng.lat
-                                ? latLng
-                                : {
-                                    lat: userPosition.lat,
-                                    lng: userPosition.lng,
-                                }
-                        }
-                    />
-                </GoogleMap>
-            </LoadScript>
-        </div>
-    );
+          <Marker
+            onDragEnd={event => {
+              setBusinessLatlng({
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+              });
+            }}
+            draggable={true}
+            position={
+              latLng.lat
+                ? latLng
+                : {
+                    lat: userPosition.lat,
+                    lng: userPosition.lng,
+                  }
+            }
+          />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
 }
 
 export default ChooseLocation;
