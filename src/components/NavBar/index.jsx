663c0741dcc9d5f1b9f88c478cf6370736f47508
hadeel11 <faour.hadeel@gmail.com>
2020-03-31 15:00:15 +0300

    creates side bar relates#49

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,95 +5,70 @@ import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import Menu from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 import Cookies from 'universal-cookie';
+import MenuSideBar from '../MenuSideBar';
 
 export default function NavBar(props) {
-    const history = useHistory();
-    const [lang, setLang] = useState('ar');
-    const [logged, setLogged] = useState(false);
+  const history = useHistory();
+  const [lang, setLang] = useState('ar');
+  const [logged, setLogged] = useState(false);
+  const [showMenuSideBar, setMenuShowSideBar] = useState(false);
 
-    const handleLang = ({ target }) => {
-        localStorage.setItem('language', target.value);
-        setLang(target.value);
-    };
+  const handleLang = ({ target }) => {
+    localStorage.setItem('language', target.value);
+    setLang(target.value);
+  };
 
-    const handleLogout = () => {
-        setLogged(false);
-        const cookies = new Cookies();
-        cookies.remove('wajjbat_access_token');
-        history.push('/');
-    };
+  useEffect(async () => {
+    const currentLang = localStorage.getItem('language') || lang;
+    setLang(currentLang);
+    try {
+      const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/api/isLoggedIn`, {
+        withCredentials: true
+      });
 
-    const handleUser = () => {
-        if (logged) return history.push('/profile');
+      if (data.id) setLogged(true);
+      return 1;
+    } catch (error) {
+      console.log(error);
+      return 1;
+    }
+  }, []);
 
-        return history.push('/signin');
-    };
+  useEffect(() => {
+    props.setLang(lang);
+  }, [lang, props]);
 
-    useEffect(async () => {
-        const currentLang = localStorage.getItem('language') || lang;
-        setLang(currentLang);
-        try {
-            const { data } = await axios.get(
-                `${process.env.REACT_APP_API_URL}/api/isLoggedIn`,
-                {
-                    withCredentials: true,
-                },
-            );
+  const useStyles = makeStyles({
+    root: { color: '#21b5a2', height: '40px', width: '40px' }
+  });
 
-            if (data.id) setLogged(true);
-            return 1;
-        } catch (error) {
-            console.log(error);
-            return 1;
-        }
-    }, []);
+  const classes = useStyles();
 
-    useEffect(() => {
-        props.setLang(lang);
-    }, [lang, props]);
+  return (
+    <div className="navBar">
+      <img onClick={() => history.push('/')} className="logo-img" src={require('../../assets/icons/logo-3.png')} alt="Logo image" />
 
-    const useStyles = makeStyles({
-        root: { color: '#21b5a2', height: '40px', width: '40px' },
-    });
-
-    const classes = useStyles();
-
-    return (
-        <div className="navBar">
-            <img
-                onClick={() => history.push('/')}
-                className="logo-img"
-                src={require('../../assets/icons/logo-3.png')}
-                alt="Logo image"
-            />
-
-            <div className="login">
-
-                <div className="changeLanguage">
-                    <select className="select" onChange={handleLang} value={lang}>
-                        <option value="ar">ar</option>
-                        <option value="en">en</option>
-                    </select>
-                </div>
-                <button onClick={() => handleUser()}>
-                    <AccountCircleIcon
-                        classes={{
-                            root: classes.root,
-                        }}
-                    />
-                </button>
-                {logged && (
-                    <button onClick={() => handleLogout()}>
-                        <ExitToAppOutlinedIcon
-                            classes={{
-                                root: classes.root,
-                            }}
-                        />
-                    </button>
-                )}
-            </div>
+      <div className="login">
+        <div className="changeLanguage">
+          <select className="select" onChange={handleLang} value={lang}>
+            <option value="ar">ar</option>
+            <option value="en">en</option>
+          </select>
         </div>
-    );
+        <button onClick={() => setMenuShowSideBar(!showMenuSideBar)}>
+          <Menu
+            classes={{
+              root: classes.root
+            }}
+          />
+        </button>
+      </div>
+      <div>
+        <MenuSideBar setMenuShowSideBar={setMenuShowSideBar} showMenuSideBar={showMenuSideBar} logged={logged} setLogged={setLogged} />
+      </div>
+    </div>
+  );
 }
