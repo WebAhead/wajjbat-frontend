87737fc62b543008f22d1fc350f9b69576d44065
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-04-01 15:31:27 +0300

    get data from BE re; #58

--- src/pages/FollowersPage/components/FollowersList/index.jsx
+++ src/pages/FollowersPage/components/FollowersList/index.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from 'react';
 import { makeStyles, List , ListItem , ListItemText , ListItemAvatar, Avatar, Divider} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
@@ -9,88 +7,27 @@ const useStyles = makeStyles((theme) => ({
         margin: 'auto',
         maxWidth: 960,
         backgroundColor: theme.palette.background.paper,
-    },
+    }
 }));
 
-const endPointUrl = process.env.REACT_APP_API_URL;
-
-export default function InsetDividers({businessid}) {
+export default function InsetDividers({businessFollowersData}) {
     
     const classes = useStyles();
-    const [followersInfo, setFollowersInfo] = useState(null);
-
-    useEffect(() => {
-        (async function getFollowers() {
-            try {
-                const { data } = await axios.get(`${endPointUrl}/api/followers/${businessid}`);
-                setFollowersInfo(data.followers);
-                console.log(data);
-                
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }());
-    },[])
-
-    if(!followersInfo){
-        return (
-            <div className="emptyBusinessDetails">
-                <CircularProgress disableShrink />
-            </div>
-        );
-    }
 
     return (
         <List className={classes.root}>
-
-            {followersInfo.map(follower => (
+            {businessFollowersData.map(follower => (
                 <div>
                     <ListItem>
                         <ListItemAvatar>
                             <Avatar src="https://creativeartworksblog.files.wordpress.com/2019/02/horseanddog-jeffjett.jpeg?w=754&h=754" />
                         </ListItemAvatar>
-                        <ListItemText primary={`Mahmod Mhamed ID ${follower.id}`} secondary={`Since: ${follower.created_at.split('T')[0]}`} />
+                        <ListItemText primary={`Mahmod Mhamed ID ${follower.id}`} secondary={`Since: ${follower.created_at}`} />
                     </ListItem>
                     <Divider variant="inset" component="li" />
                 </div>
             )
             )}
-
-
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar src="https://creativeartworksblog.files.wordpress.com/2019/02/horseanddog-jeffjett.jpeg?w=754&h=754" />
-                </ListItemAvatar>
-                <ListItemText primary="Mahmod Mhamed" secondary="Since: Jan 9, 2014" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-
-
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=804" />
-                </ListItemAvatar>
-                <ListItemText primary="Najwan Abu Saleh" secondary="Since: Jan 9, 2014" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Maruti-Alto-800/7066/1565265450182/front-left-side-47.jpg?tr=h-140" />
-                </ListItemAvatar>
-                <ListItemText primary="Rabea Fahoum" secondary="Since: Jan 9, 2014" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar src="https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-760w.JPG" />
-                </ListItemAvatar>
-                <ListItemText primary="Mario Saliba" secondary="Since: Jan 9, 2014" />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-
         </List>
     );
 }
